31f71505166142a85312ec3e6a7b1e9796b78208
bilalmk <bilalmk@gmail.com>
2025-01-13 13:27:45 +0500

    fix code

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 export default nextConfig;
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
->>>>>>> 275f5c0002fab35f986307fb1d859f20bf7a35b5
